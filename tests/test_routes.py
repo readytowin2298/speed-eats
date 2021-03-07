e41b9515929802763c9ecd565674a49d91b75a74
= <readytowin2298@gmail.com>
2021-03-06 19:31:50 -0600

    worked on test files

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -7,4 +7,17 @@ app.config['TESTING'] = True
 
 class TestRoutes(TestCase):
 
-    
+    # executed prior to each test
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['DEBUG'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres:///speed_eats_test' + \
+            os.path.join(app.config['BASEDIR'], TEST_DB)
+        self.app = app.test_client()
+        db.drop_all()
+        db.create_all()
+ 
+        # Disable sending emails during unit testing
+        mail.init_app(app)
+        self.assertEqual(app.debug, False)
