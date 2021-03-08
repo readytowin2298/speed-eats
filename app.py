a1e2ea68421e7bfb874696f6017928c41d785e7f
= <readytowin2298@gmail.com>
2021-03-08 03:37:32 -0600

    beefed up testing

--- app.py
+++ app.py
@@ -18,6 +18,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
 
+# app.config['WTF_CSRF_ENABLED'] = False
+# Disable above if not in testing
+
 connect_db(app)
 
 @app.before_request
--- test_routes.py
+++ test_routes.py
@@ -1,4 +1,4 @@
-from app import app, db, User, Party, Resturaunt, Vote, UserForm
+from app import app, db, User, Party, Resturaunt, Vote, PartyForm, UserForm, LoginForm
 from unittest import TestCase
 import random
 
@@ -22,7 +22,7 @@ app.config['WTF_CSRF_ENABLED'] = False
 #         mail.init_app(app)
 #         self.assertEqual(app.debug, False)
 
-class UserViewsTestCase(TestCase):
+class TotalTestCase(TestCase):
     """Tests for views for Users."""
 
     def setUp(self):
@@ -106,7 +106,73 @@ class UserViewsTestCase(TestCase):
             html = resp.get_data(as_text=True)
 
         self.assertIn(f"""<a href="" class="nav-link">{leader_name}</a>""", html)
-            
-        
+
+    def test_create_party(self):
+        correct_count = (len(Party.query.all()) + 1)
+        leader_id = self.party.leader.id
+
+        with app.app_context():
+            form = PartyForm()
+            form.name.data = 'Test'
+            form.address.data = '3112 E Broad St'
+            form.city.data = 'Mansfield'
+            form.state.data = 'Texas'
+            form.zip_code.data ='76063'
+        with app.test_client() as client:
+            with client.session_transaction() as sess:
+                sess['curr_user'] = leader_id
+            resp = client.post('/parties/create', data=form.data)
+        self.assertEqual(len(Party.query.all()), correct_count)
+        self.assertEqual(resp.status_code, 302)
+    
+    def test_delete_party(self):
+        party_id = self.party.id
+        leader_id = self.party.leader.id  
+
+        with app.test_client() as client:
+            with client.session_transaction() as sess:
+                sess['curr_user'] = leader_id
+            resp = client.post(f"/parties/delete/{party_id}", follow_redirects=True)
+            html = resp.get_data(as_text=True)
+
+        self.assertEqual(len(Party.query.all()), 0)   
+        self.assertIn('Party was successfully deleted', html)    
+
+    def test_login(self):
+        email="test@test.com"
+        password="testtesttest"
+        with app.app_context():
+            form = LoginForm()
+            form.email.data = email
+            form.password.data = password
+        with app.test_client() as client:
+            resp = client.post(f"/login", data=form.data, follow_redirects=True)
+            html = resp.get_data(as_text=True)
+        self.assertEqual(resp.status_code, 200)
+        self.assertIn("Johnny Doe", html)
+
+    def test_logout(self):
+        with app.test_client() as client:
+            with client.session_transaction() as sess:
+                sess['curr_user'] = self.party.leader.id
+            resp = client.get('/logout', follow_redirects=True)
+            html = resp.get_data(as_text=True)
+        self.assertIn('Come back soon!', html)
+
+    def test_signup(self):
+        email = 'bryan_hale@speedeats.com'
+        with app.app_context():
+            form = UserForm()
+            form.name.data = "Bryan"
+            form.password.data = 'supersecurepassword'
+            form.email.data = email
+        with app.test_client() as client:
+            resp = client.post('/signup', data=form.data, follow_redirects=True)
+            html = resp.get_data(as_text=True)
+        self.assertTrue(User.query.filter_by(email=email).first())
+        self.assertIn('Success! Account created!', html)
+        self.assertIn('You have been automatically logged in', html)
+
+
 
     # Through this project I have learned the importance of writing tests as you go
